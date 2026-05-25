c8fe41d171b7eef2f346ab77ca9bf7d23735d66d
louisalonsocely-cpu <louisalonzocely@gmail.com>
2026-05-24 22:33:58 -0500

    Config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://louisalonzocely-cpu.github.io',
+  base: '/portafolio',
+  devToolbar: { enabled: false },
   integrations: [tailwind()],
 });
