806fac29d52df97c53becc16f5136735871f478d
louisalonsocely-cpu <louisalonzocely@gmail.com>
2026-06-08 09:14:35 -0500

    Actualizacion a github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://luisalonsocely.netlify.app',
+  site: 'https://louisalonzocely-cpu.github.io',
+  base: '/portafolio',
   devToolbar: { enabled: false },
   integrations: [tailwind()],
 });
